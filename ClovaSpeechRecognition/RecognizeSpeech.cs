c69c363114b874b84c09426f8b3e9c733453af1c
Hyungsoo Kim <javaos74@gmail.com>
2019-04-29 18:32:59 +0900

    initial

---
+++ ClovaSpeechRecognition/RecognizeSpeech.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Activities;
+using System.IO;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using Newtonsoft;
+using Newtonsoft.Json.Linq;
+
+namespace ClovaSpeech
+{
+    public enum SupportLanguages
+    {
+        한국어,
+        中国,
+        English,
+        日本
+    }
+    public sealed class RecognizeSpeech : CodeActivity
+    {
+        private static SupportLanguages[] _languages = 
+            new SupportLanguages[] {SupportLanguages.한국어, SupportLanguages.中国, SupportLanguages.English, SupportLanguages.日本 };
+        //음성 파일 정보 
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> FilePath { get; set; }
+    
+        //음성 언어 한국어/영어/일본어/중국어 지원 
+        [Category("Input")]
+        [RequiredArgument]
+        public SupportLanguages Lang { get; set; }
+
+        //Clova API를 사용하기 위한 Client ID 
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ClientID { get; set; }
+         
+        //Clova API를 사용하기 위한 Client Secrete 
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ClientSecret { get; set; }
+
+        [Category("Output")]
+        [RequiredArgument]
+        public OutArgument<string> Speech { get; set; }
+
+        //오류 코드 값 
+        [Category("Output")]
+        [RequiredArgument]
+        public OutArgument<string> ErrorCode { get; set; }
+
+        //실제 처리하는 로직 
+        protected override void Execute(CodeActivityContext context)
+        {
+            //입력된 매개변수 정보 가져오기 
+            string filePath = context.GetValue(this.FilePath);
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new Exception( "음성파일의 경로가 없습니다.");
+            }
+            // 언어 코드 ( Kor, Jpn, Eng, Chn )
+            string lang = "Kor";
+            if (Lang == SupportLanguages.한국어)
+                lang = "Kor";
+            else if (Lang == SupportLanguages.English)
+                lang = "Eng";
+            else if (Lang == SupportLanguages.中国)
+                lang = "Chn";
+            else if (Lang == SupportLanguages.日本)
+                lang = "Jpn";
+
+            string clientId = context.GetValue(this.ClientID);
+            if (string.IsNullOrEmpty(clientId))
+            {
+                throw new Exception("Clova API를 호출하기 위한 Client ID 값이 없습니다.");
+            }
+            string clientSecret = context.GetValue(this.ClientSecret);
+            if( string.IsNullOrEmpty( clientSecret))
+            {
+                throw new Exception("Clova API를 호출하기 위한 Client Secrete 값이 없습니다.");
+
+            }
+
+            FileStream fs = null;
+            byte[] fileData = null;
+            try
+            {
+                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, fileData.Length);
+                fs.Close();
+            } catch ( IOException ioe)
+            {
+                throw new IOException(ioe.Message);
+            }
+
+            string url = $"https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang={lang}";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-NCP-APIGW-API-KEY-ID", clientId);
+            request.Headers.Add("X-NCP-APIGW-API-KEY", clientSecret);
+            request.Method = "POST";
+            request.ContentType = "application/octet-stream";
+            request.ContentLength = fileData.Length;
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(fileData, 0, fileData.Length);
+                requestStream.Close();
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            string text = reader.ReadToEnd();
+            //인식된 문장 저장 
+            System.Console.WriteLine(text);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                ErrorCode.Set(context, "OK");
+                JObject respJson= JObject.Parse(text);
+                Speech.Set(context, respJson["text"].ToString());
+            }
+            else
+            {
+                JObject respJson = JObject.Parse(text);
+                ErrorCode.Set(context,  respJson["error"]["errorCode"].ToString());
+                Speech.Set(context, respJson["error"]["message"].ToString());
+            }
+            stream.Close();
+            response.Close();
+            reader.Close();
+        }
+    }
+}
---
+++ ClovaSpeechRecognition/SynthesisSpeech.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.ComponentModel;
+
+namespace ClovaSpeech
+{
+    public enum SpeakerSex {
+        Female = 0,
+        Male
+    };
+    public enum SpeakLanguages
+    {
+        한국어 = 0,
+        English,
+        中国,
+        日本,
+        Spain
+    }
+
+    public sealed class SynthesisSpeech : CodeActivity
+    {
+         private static string[,] speakers = new string[5, 2] { {"mijin", "jinho"}, 
+                                                    {"clara","matt"},
+                                                    {"meimei","liangliang"},
+                                                    {"shinji","shinji"},
+                                                    {"carmen", "jose"} }; 
+        //음성으로 변활된 텍스트 정보 
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> Text { get; set; }
+        
+        //음성 언어 한국어/영어/일본어/중국어/스페인어  지원 
+        [Category("Input")]
+        [RequiredArgument]
+        public SpeakLanguages Lang { get; set; }
+
+        //남성/여성 구분자 
+        [Category("Input")]
+        [RequiredArgument]
+        public SpeakerSex Sex { get; set; }
+
+        //Clova API를 사용하기 위한 Client ID 
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ClientID { get; set; }
+
+        //Clova API를 사용하기 위한 Client Secrete 
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> ClientSecret { get; set; }
+
+        //음성으로 전환된 파일 경로 
+        [Category("Output")]
+        [RequiredArgument]
+        public OutArgument<string> FilePath{ get; set; }
+
+        [Category("Output")]
+        [RequiredArgument]
+        public OutArgument<string> ErrorCode{ get; set; }
+
+        //음성합성 로직 처리 
+        protected override void Execute(CodeActivityContext context)
+        {
+            //입력 받은 매개변수 가져오기 
+            string text = context.GetValue(this.Text);
+            // 화자 - 언어 및 성별 구분 
+            string speaker = speakers[ (int)Lang, (int)Sex];        
+            string clientId = context.GetValue(this.ClientID);
+            string clientSecret = context.GetValue(this.ClientSecret);
+
+            string url = "https://naveropenapi.apigw.ntruss.com/voice/v1/tts";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-NCP-APIGW-API-KEY-ID", clientId);
+            request.Headers.Add("X-NCP-APIGW-API-KEY", clientSecret);
+            request.Method = "POST";
+            var querystr = string.Format("speaker={0}&speed=0&text={1}", speaker, text);
+            System.Console.WriteLine("POST BODY : " + querystr);
+            byte[] byteDataParams = Encoding.UTF8.GetBytes(querystr);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteDataParams.Length;
+            Stream st = request.GetRequestStream();
+            st.Write(byteDataParams, 0, byteDataParams.Length);
+            st.Close();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            System.Console.WriteLine("Status : " + response.StatusCode.ToString());
+            if( response.StatusCode == HttpStatusCode.OK)
+            {
+                var tempPath = Path.GetTempFileName();
+                tempPath = tempPath.Replace(".tmp", ".mp3");
+                using (Stream output = File.OpenWrite(tempPath))
+                using (Stream input = response.GetResponseStream())
+                {
+                    input.CopyTo(output);
+                }
+                FilePath.Set(context, tempPath);
+                ErrorCode.Set(context, "OK");
+                MediaPlayer.MediaPlayerClass _player;
+                _player = new MediaPlayer.MediaPlayerClass();
+                _player.FileName = tempPath;
+                _player.Play();
+            }
+            else
+            {
+                FilePath.Set(context, "");
+                ErrorCode.Set(context, "");
+            }
+        }
+    }
+}
